13fe63eafd6231d937db78cb1d1ad4551037697c
MalkiMahedani <136500247+MalkiMahedani@users.noreply.github.com>
2023-07-29 13:53:08 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailto = "malkimahedani13@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+	
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: http://127.0.0.1:51865/preview/app/index.html?mailsend");
+ }
